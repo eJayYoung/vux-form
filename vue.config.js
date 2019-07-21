444cb9ccef423193b87396d360099d74f40704da
eJay <ejay_young@163.com>
2019-07-21 23:25:26 +0800

    feat: add travis for auto deploy

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,9 @@
 const vuxLoader = require('vux-loader')
 
 module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? 'vux-form'
+  : '/',
   configureWebpack: config => {
     config.entry.app = './public/main.js'
     vuxLoader.merge(config, {
